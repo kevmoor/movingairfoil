d782dde9e41ee087a1cdae46b20db80a39638be0
Kevin Moore <kevmoor@login07.frontier.olcf.ornl.gov>
2024-03-08 11:41:48 -0500

    Fix multi_submit to submit in this year

--- multi_submit.sh
+++ multi_submit.sh
@@ -3,12 +3,15 @@
 current_time=$(date +%s)
 interval=$((2 * 3600)) # 2 hours in sec
 
+current_time_formatted=$(date -d @$current_time "+%Y-%m-%dT%H:%M:%S")
+echo "Current time: $current_time_formatted"
+
 for i in {1..5}; do
-    job_start=$((current_time + (i - 1) * interval))
-    job_start_formatted=$(date -d @$job_start "+%Y-%m-%d %H:%M:%S")
+    job_start=$((current_time + (i - 1) * interval+30))
+    job_start_formatted=$(date -d @$job_start "+%Y-%m-%dT%H:%M:%S")
 
     echo "Submitting job $i to start at $job_start_formatted"
 
-    sbatch --begin=now+${job_start-current_time}seconds runtest.slurm
+    sbatch --begin=${job_start_formatted} runtest.slurm
 
 done
