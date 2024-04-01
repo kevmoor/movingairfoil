c6a57237b2b084d18302e6c897b3bf9e07abec41
kevmoor <kevmoor@sandia.gov>
2024-04-01 13:53:37 -0600

    Revert rotation length

--- naca/wiggle.py
+++ naca/wiggle.py
@@ -1,10 +1,10 @@
 import numpy as np
 
-timestep    = 0.5
+timestep    = 0.0005
 steadysteps = 200
 wiggleper   = 0.146
 wiggleamp   = 8.2 * np.pi/180
-totalsteps  = 20
+totalsteps  = 20000
 
 wigglefreq = 2*np.pi/wiggleper
 steadytime = steadysteps * timestep
